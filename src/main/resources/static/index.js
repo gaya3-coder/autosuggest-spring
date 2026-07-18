bfe51cbc34428942c5dd281be1a686bc68f221f1
gayathri <25071A6282@vnrvjiet.in>
2026-07-18 12:50:23 +0530

    random user

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -5,12 +5,12 @@ console.log("hello from JS!")
 
 var users = [
     {
-        "name" : "John Doe",
+        "name" : "John Pattinson",
         "gender" : "Male",
         "img" : "john.png"
     },
     {
-        "name" : "Jane Doe",
+        "name" : "Jane Cavendish",
         "gender" : "Female",
         "img" : "jane.png"
     }
@@ -33,5 +33,34 @@ function toggleUser(){
     // 3: gender
     var userGender = document.getElementById("user-gender");
     userGender.innerHTML = users[id].gender;
+
+}
+function randomUser(){
+    fetch('https://randomuser.me/api/')
+         .then(function(response){
+            return response.json();
+         })
+         .then(function(data){
+
+            var userData = data.results[0];
+
+    // 1: image
+    var userImage = document.getElementById("user-image");
+    userImage.src = userData.picture.large;
+
+    // 2: name
+    var userName = document.getElementById("user-name");
+    userName.innerHTML = userData.name.first + " " + 
+                                 userData.name.last;
+
+    // 3: gender
+    var userGender = document.getElementById("user-gender");
+    userGender.innerHTML = userData.gender;
+
+
+         })
+         .catch(function(error){
+            console.log(err);
+         })
 
 }
